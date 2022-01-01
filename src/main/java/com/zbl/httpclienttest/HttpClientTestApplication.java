2f04287d7b2c8b3e00d6fc00d02946f7ea262c14
赵保良 <zhaobaoliang@zhaobaoliangdeMacBook-Pro.local>
2022-01-01 23:22:19 +0800

    项目新建

---
+++ src/main/java/com/zbl/httpclienttest/HttpClientTestApplication.java
@@ -0,0 +1,13 @@
+package com.zbl.httpclienttest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpClientTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpClientTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zbl/httpclienttest/HttpClientTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zbl.httpclienttest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
